d1929e75731c32aa1803d68bc6f301e7b4598d19
Damian S <50520717+Damian77504@users.noreply.github.com>
2021-11-25 13:04:49 +0100

    Add files via upload

---
+++ FilesUpdate/ui.R
@@ -0,0 +1,26 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Analysis of Ozone based on Wind"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("sliderWind", "What is the Wind [mph] value?", 1, 21, value = 10),
+            checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
+            checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
+        ),
+        mainPanel(
+            plotOutput("plot1"),
+            h3("Ozone Prediction from Model 1:"),
+            textOutput("pred1"),
+            h3("Ozone Prediction from Model 2:"),
+            textOutput("pred2"),
+        ))))
